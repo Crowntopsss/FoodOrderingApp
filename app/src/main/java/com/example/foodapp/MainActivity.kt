a0f82af94ba2c34bafaeb26a8fa94efa74b4a682
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-08-13 15:51:47 +0100

    Added ordering activity

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.foodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.foodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up button click listener
+        binding.getStartedButton.setOnClickListener {
+            // Create an Intent to navigate to the NextActivity
+            val intent = Intent(this, OrderActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
